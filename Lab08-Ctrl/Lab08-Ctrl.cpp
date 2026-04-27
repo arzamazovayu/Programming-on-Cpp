1c3f11c7df028e0977b5524b714aa57ed56cf9db
arzamazovayu <alexarzamazov95@gmail.com>
2026-04-27 23:12:10 +0300

    Create Lab08-Ctrl.cpp

---
+++ Lab08-Ctrl/Lab08-Ctrl.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "Time.h"
+
+Time set_time() {
+	
+	int h;
+	int m;
+	int s;
+
+	std::cout << "\nВведите количество часов: ";
+	std::cin >> h;
+	std::cout << "\nВведите количество минут: ";
+	std::cin >> m;
+	std::cout << "\nВведите количество секунд: ";
+	std::cin >> s;
+
+	return Time(h, m, s);
+}
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+	std::cout << "Реализация класса Time.\n";
+
+	std::cout << "Ввод Time1\n";
+	Time time1 = set_time();
+	std::cout << "Значения Time1:\n";
+	time1.get_time();
+
+	std::cout << "Ввод Time2\n";
+	Time time2 = set_time();
+	std::cout << "Значения Time2:\n";
+	time2.get_time();
+
+	Time time3 = time3.sum_time(time1, time2);
+
+	std::cout << "Значения суммы Time1 и Time2:\n";
+	time3.get_time();
+
+	return 0;
+}
